31b17620df68acbc93eea1b0d8e7983d3e24a833
Frantz Lindor <lindorfrantz17@gmail.com>
2020-12-14 11:53:58 -0600

    Mobile now works

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const https = require("https");
 
-const port = 3000
+const port = 3001
 
 
 app.use(express.static("public"))
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
-app.listen(process.env.PORT || 3000, () => console.log(`Server is running in port 3000`))
+app.listen(process.env.PORT || 3001, () => console.log(`Server is running in port 3001`))
