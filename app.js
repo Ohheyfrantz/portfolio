221a63994ab939a57f06580231961751567023f9
Frantz Lindor <lindorfrantz17@gmail.com>
2022-01-05 12:40:43 -0600

    updated header images and adding blog page

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const https = require("https");
 const port = 3001
 
 
-app.use(express.static("public"))
+app.use(express.static("/public"));
 
 app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
 app.listen(process.env.PORT || 3001, () => console.log(`Server is running in port 3001`))
